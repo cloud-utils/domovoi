79e19c68441071d441c04089e06a9d7197f11b66
Andrey Kislyuk <kislyuk@gmail.com>
2018-08-29 14:38:53 -0700

    v1.7.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="domovoi",
-    version="1.7.8",
+    version="1.7.9",
     url='https://github.com/kislyuk/domovoi',
     license='Apache Software License',
     author='Andrey Kislyuk',
